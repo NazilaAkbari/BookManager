632f5d1a97bca2c5a12a50deba05a6c054b11b90
nazi <nazila.akbari87@gmail.com>
2015-01-21 23:05:15 +0330

    test for edit and delete written

--- BookWeb/src/test/java/com/nazi/controller/BookControllerTest.java
+++ BookWeb/src/test/java/com/nazi/controller/BookControllerTest.java
@@ -3,17 +3,24 @@ package com.nazi.controller;
 import org.hamcrest.Matchers;
 import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.nazi.model.Book;
+import com.nazi.model.User;
+import com.nazi.persistence.BookRepository;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.junit.Assert.*;
 
 @Transactional
 public class BookControllerTest extends BaseWebTest {
+
+	@Autowired
+	BookRepository bookRepository;
 
 	@Test
 	public void userIndex() throws Exception {
@@ -43,27 +50,56 @@ public class BookControllerTest extends BaseWebTest {
 
 	@Test
 	public void saveBook() throws Exception {
-		mockMvc.perform(post("/saveBook").session(loginAsUser())
-			   .param("name", "mohakeme")
-			   .param("author", "kafka")
-			   .param("readStatus", "1"))
-			   .andExpect(status().isOk());
-		
+		mockMvc.perform(
+				post("/saveBook").session(loginAsUser())
+						.param("name", "mohakeme").param("author", "kafka")
+						.param("readStatus", "1")).andExpect(status().isOk());
+
 		mockMvc.perform(get("/getBooks").session(loginAsUser()))
-		       .andExpect(status().isOk()).andDo(print())
-		       .andExpect(jsonPath("$").isArray())
-		       .andExpect(jsonPath("$", Matchers.hasSize(1)));
+				.andExpect(status().isOk()).andDo(print())
+				.andExpect(jsonPath("$").isArray())
+				.andExpect(jsonPath("$", Matchers.hasSize(1)));
+
+	}
+
+	@Test
+	public void saveEditBook() throws Exception {
+		Book book = new Book();
+		book.setName("mohakeme");
+		book.setAuthor("kafka");
+		book.setReadStatus(1);
+		Book newBook = bookRepository.save(book);
+
+		mockMvc.perform(
+				post("/saveEditBook").session(loginAsUser())
+						.param("id", newBook.getId().toString())
+						.param("name", "havie").param("author", "khosravi")
+						.param("readStatus", "1")).andExpect(status().isOk());
+		
+		Book editedBook = bookRepository.findOne(newBook.getId());
+
+		assertEquals("havie", editedBook.getName());
+		assertEquals("khosravi", editedBook.getAuthor());
+		assertEquals(1, editedBook.getReadStatus());
+		
 
 	}
 	
 	@Test
-	public void saveEditBook() throws Exception{
-		mockMvc.perform(post("/saveEditBook").session(loginAsUser())
-			   .param("id","1")
-			   .param("name","havie")
-			   .param("author", "khosravi")
-			   .param("readStatus", "1"))
-			   .andExpect(status().isOk());
+	public void deleteBook() throws Exception{
+		Book book = new Book();
+		book.setName("mohakeme");
+		book.setAuthor("kafka");
+		book.setReadStatus(1);
+		User user = new User();
+		user.setUsername("nazila.akbari87@gmail.com");
+		book.setUser(user);
+		Book newBook = bookRepository.save(book);
+		mockMvc.perform(get("/deleteBook").session(loginAsUser())
+				.param("id", newBook.getId().toString()))
+				.andExpect(status().isOk());
+		
+		
 	}
 
 }
